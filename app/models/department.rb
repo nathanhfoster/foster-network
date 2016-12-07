5cd99a5217c215ba30876e31ac703d00ce44418f
Nathan <nateinthegame@gmail.com>
2016-12-06 18:05:05 -0800

    Heroku ready

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,2 +1,6 @@
 class Department < ApplicationRecord
+	attr_accessor :dNname
+
+
+	validates :d_name, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206235639) do
+ActiveRecord::Schema.define(version: 20161207013432) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "department_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20161206235639) do
   create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "reviews", force: :cascade do |t|
